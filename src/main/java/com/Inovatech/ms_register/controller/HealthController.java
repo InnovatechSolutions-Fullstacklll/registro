92659cb40e4ede7aad4c6c55c780bef6deafc090
camilo <cc.leiva@duocuc.cl>
2026-05-08 04:14:39 -0400

    ayuda

---
+++ src/main/java/com/Inovatech/ms_register/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.Inovatech.ms_register.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "ms-register funcionando";
+    }
+}
+
+//ESTE ES PARA PROBAR
